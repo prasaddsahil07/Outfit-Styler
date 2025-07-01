1119ae5174792d3b072c05a8e6990c6fce70dd3a
Sahil Prasad <sahilprasad41969@gmailcom>
2025-07-01 17:25:15 +0530

    routes also modified

--- routes/zuriMagazine.routes.js
+++ routes/zuriMagazine.routes.js
@@ -1,9 +1,26 @@
-import {Router} from 'express';
-import { getArticlesByCategory, getAllCategories  } from "../controllers/zuriMagazine.controllers.js";
+import { Router } from 'express';
+import {
+  getAllCategories,
+  getArticlesByCategory,
+  getAllArticles,
+  addArticle,
+  updateArticle,
+  deleteArticle,
+  getArticleById
+} from "../controllers/zuriMagazine.controllers.js";
+import { upload } from '../middleware/multer.middleware.js';
 
 const router = Router();
 
+// USER-FACING ROUTES
 router.get('/allCategories', getAllCategories);
 router.get('/articlesByCategory', getArticlesByCategory);
+router.get('/allArticles', getAllArticles);
+router.get('/article/:id', getArticleById);
+
+// ADMIN ROUTES
+router.post('/addArticle', upload.single("bannerImage"), addArticle);
+router.put('/updateArticle/:id', upload.single("bannerImage"), updateArticle);
+router.delete('/deleteArticle/:id', deleteArticle);
 
 export default router;
