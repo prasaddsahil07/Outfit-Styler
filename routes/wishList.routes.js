632dfd9a436869c28f23a28889dfd0515b9228ab
Sahil Prasad <sahilprasad41969@gmailcom>
2025-07-07 12:56:19 +0530

    import statement fixed

--- routes/wishList.routes.js
+++ routes/wishList.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { toggleWishlistItem, getWishlistItems } from '../controllers/wishlist.controller.js';
-import { verifyJWT, verifyToken } from '../middleware/auth.middleware.js';
+import { toggleWishlistItem, getWishlistItems } from '../controllers/wishList.controllers.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/toggle', verifyToken, toggleWishlistItem);
+router.post('/toggle', verifyJWT, toggleWishlistItem);
 router.get('/show', verifyJWT, getWishlistItems);
 
 export default router;
